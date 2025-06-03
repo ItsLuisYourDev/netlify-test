7f3a4ac607df10f8165e20c68651c16e4d8425e5
ItsLuisYourDev <antocede5@gmail.com>
2025-06-03 00:24:28 -0500

    Fix Netlify deployment configuration

--- functions/server.js
+++ functions/server.js
@@ -3,8 +3,20 @@ const serverless = require('serverless-http');
 
 const app = express();
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.json({ message: '¡Hola Mundo!' });
+});
+
+// Handle API requests
+app.get('/api/*', (req, res) => {
+  res.json({ message: '¡Hola Mundo desde la API!' });
 });
 
 module.exports.handler = serverless(app); 
